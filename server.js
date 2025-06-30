fcadf2bb607812c88c9398a7d157156e18f4015e
Musammil N <mohammedmusammiln@gmail.com>
2025-06-30 19:54:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from React app
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Handle React routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
